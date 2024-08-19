907b6a4a29c2b7bcdf8af49700dd1a97b5acd882
Alexander <krupich.alexander@gmail.com>
2024-08-19 09:30:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome():
+    return 'Главная страница.'
+
+
+@app.get('/user/admin')
+async def admin():
+    return 'Вы вошли как администратор'
+
+
+@app.get('/user/{user_id}')
+async def user_id(user_id):
+    return f'Вы вошли как пользователь № {user_id}'
+
+
+@app.get('/user')
+async def user_id(username: str = 'Noname', age: int = '0'):
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
